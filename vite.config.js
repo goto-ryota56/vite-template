b2dcacbf3cc8257425624dc26693a9768e7ba0f4
k.takaya <k.takaya@marukinad.co.jp>
2023-04-11 18:15:19 +0900

    ffff

--- vite.config.js
+++ vite.config.js
@@ -7,65 +7,65 @@ const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
-   root,
-   base: './',
-   server: {
-      port: 1234,
-      open: true,
-   },
-   build: {
-      outDir,
-      emptyOutDir: true,
-      modulePreload: {
-         polyfill: false,
+  root,
+  base: './',
+  server: {
+    port: 1234,
+    open: true
+  },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    modulePreload: {
+      polyfill: false
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about/index.html')
       },
-      rollupOptions: {
-         input: {
-            main: resolve(root, 'index.html'),
-            about: resolve(root, 'about/index.html'),
-         },
-         output: {
-            assetFileNames: assetInfo => {
-               let extType = assetInfo.name.split('.').at(1);
-               if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                  extType = 'images';
-               }
-               return `assets/${extType}/[name][extname]`;
-            },
-            entryFileNames: `assets/js/bundle.js`,
-         },
+      output: {
+        assetFileNames: assetInfo => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        entryFileNames: `assets/js/bundle.js`
+      }
+    }
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(root, 'common/components')
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
       },
-   },
-   plugins: [
-      handlebars({
-         partialDirectory: resolve(root, 'common/components'),
-      }),
-      viteImagemin({
-         gifsicle: {
-            optimizationLevel: 7,
-            interlaced: false,
-         },
-         optipng: {
-            optimizationLevel: 7,
-         },
-         mozjpeg: {
-            quality: 20,
-         },
-         pngquant: {
-            quality: [0.8, 0.9],
-            speed: 4,
-         },
-         svgo: {
-            plugins: [
-               {
-                  name: 'removeViewBox',
-               },
-               {
-                  name: 'removeEmptyAttrs',
-                  active: false,
-               },
-            ],
-         },
-      }),
-   ],
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    })
+  ]
 });
