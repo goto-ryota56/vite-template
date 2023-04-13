ca3cfe62fcb0387502f628527394ac8ba088e621
k.takaya <k.takaya@marukinad.co.jp>
2023-04-13 17:12:21 +0900

    vite

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-   plugins: {
-      autoprefixer: {},
-      'postcss-sort-media-queries': {},
-      'css-declaration-sorter': { order: 'smacss' },
-      '@fullhuman/postcss-purgecss': {
-         content: ['./src/**/*.html', './src/js/**/*.js'],
-         safelist: ['hoge'],
-      },
-   },
+  plugins: {
+    autoprefixer: {},
+    "postcss-sort-media-queries": {},
+    "css-declaration-sorter": { order: "smacss" },
+    "@fullhuman/postcss-purgecss": {
+      content: ["./src/**/*.html", "./src/js/**/*.js"],
+      safelist: ["hoge"],
+    },
+  },
 };
