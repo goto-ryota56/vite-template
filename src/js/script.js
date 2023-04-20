0367233273d8c6116e0bf033e1e544aa80f28532
k.takaya <k.takaya@marukinad.co.jp>
2023-04-20 09:25:37 +0900

    vite更新

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 import $ from "jquery";
 import gsap from "gsap";
 
-gsap.to(".aaa", 0.3, { opacity: 0.3 });
-document.querySelector(".aaa");
 $(".hello").on("click", function () {
-  $(".world").fadeToggle();
+  gsap.to(".world", 0.3, { opacity: 0 });
 });
